149b2ab365df7169a479adf93a5205330f71d790
master <ehtiramabdullayev@gmail.com>
2020-02-26 23:30:40 +0100

    -first commit

---
+++ src/main/java/com/hotel/reception/HotelReceptionApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.reception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelReceptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelReceptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel/reception/HotelReceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.reception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelReceptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

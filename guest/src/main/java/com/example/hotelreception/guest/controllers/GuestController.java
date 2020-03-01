d1417531fae7f76e06cc9ff6113d535ad165f233
master <ehtiramabdullayev@gmail.com>
2020-03-01 17:26:31 +0100

    added mail-package module

--- guest/src/main/java/com/example/hotelreception/guest/controllers/GuestController.java
+++ guest/src/main/java/com/example/hotelreception/guest/controllers/GuestController.java
@@ -3,7 +3,6 @@ package com.example.hotelreception.guest.controllers;
 import com.example.hotelreception.guest.api.model.response.GuestResponse;
 import com.example.hotelreception.guest.models.Guest;
 import com.example.hotelreception.guest.service.GuestService;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
